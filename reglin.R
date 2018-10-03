916ac4851ca6d922dfda9dd0b9a98ba3cc4d615d
alizeepr <alizee.poirierrenard@gmail.com>
2018-10-03 16:03:46 +0200

    derniere version

--- reglin.R
+++ reglin.R
@@ -1,4 +1,12 @@
 confoodDF <- as.data.frame(confood2)
+cf<-read.table(file = 'C:/Users/alizee/Downloads/finaltab.tsv', sep = '\t', header = TRUE)
+cf_agg <- confoodDF %>% group_by(weeknum) 
+sum = summarise(cf_agg, saleb1 = sum(saleb1), saleb2 = sum(saleb2), saleb4 = sum(saleb4), saleb3 = sum(saleb3),
+                saleb5 = sum(saleb5), apriceb1 = mean(apriceb1), apriceb2 = mean(apriceb2), apriceb3 = mean(apriceb3),
+                apriceb4= mean(apriceb4), apriceb5 = sum(apriceb5), rpriceb1 = mean(rpriceb1), rpriceb2 = mean(rpriceb2), 
+                rpriceb3 = mean(rpriceb3), rpriceb4= mean(rpriceb4), rprice5 = sum(rpriceb5), promotb1 = mean(promotb1),
+                promotb2 = mean(promotb2), promotb3 = mean(promotb3), promotb4 = mean(promotb4), 
+                promotb5 = mean(promotb5))
 
 #group of brand 2 to 5
 #sales
@@ -31,35 +39,37 @@ b<- m$coefficients[2]
 abline(a, b)
 
 #Création de données d'apprentissage et de test
-ndata<-nrow(confood1)
+ndata<-nrow(sum)
 napp<-round((2/3)*ndata)
 ntst<-round(ndata-napp)
 set.seed(100)
 dataApp<-sample(1:ndata, napp)
-data.train<-confood1[dataApp,]
-data.test<-confood1[-dataApp,]
+data.train<-sum[dataApp,]
+data.test<-sum[-dataApp,]
 
-#test predire sale1 avec toutes les variables
+#test predire saleb1 avec toutes les variables
 reg = lm(saleb1 ~ apriceb1+rpriceb1+salesOmean+period+storenum+weeknum+apriceOmean+promotb1, data=data.train)
+reg = lm(saleb1 ~. -saleb1, data=data.train)
 res<-predict(reg, newdata=data.test)
 plot(data.test$saleb1, res)
 err_quad <- mean((data.test$saleb1-res)^2)
 
 summary(reg)
 
+sum <- subset(sum, !(sum$saleb1==58525))
+sum <- subset(sum, !(sum$saleb1==34270))
+
 #test avec les variables les plus significatives seulement
-reg = lm(saleb1 ~ apriceb1+rpriceb1+apriceOmean+promotb1, data=data.train)
+reg = lm(saleb1 ~ apriceb1+rpriceb1+promotb1, data=data.train)
 res<-predict(reg, newdata=data.test)
 plot(data.test$saleb1, res)
 err_quad <- mean((data.test$saleb1-res)^2)
 
 #test avec les variables les plus significatives seulement
-reg = lm(saleb1 ~ apriceb1+rpriceb1, data=data.train)
+reg = lm(saleb1 ~ rpriceOmean+storenum+weeknum+rpriceb1+apriceOmean, data=data.train)
 res<-predict(reg, newdata=data.test)
 plot(data.test$saleb1, res)
 err_quad <- mean((data.test$saleb1-res)^2)
-
-#Validation
 
 
 #change promotion variable to dummy
@@ -74,7 +84,7 @@ corrplot(M, method='color')
 
 #acp
 library(factoextra)
-res.pca <- prcomp(confood1, scale = TRUE)
+res.pca <- prcomp(sum, scale = TRUE)
 fviz_eig(res.pca)
 # 3 ou 4 variables suffisent a expliquer, méthode du coude
 fviz_pca_ind(res.pca,
@@ -96,7 +106,8 @@ eig.val
 
 library("corrplot")
 corrplot(res.var$cos2, is.corr=FALSE)
-#on remarque que la combinaison rpriceOmean, apriceOmean et storenum
+M <- cor(sum)
+corrplot(M, method='color')
 
 #autre methode prcomp
 prin_comp <- prcomp(data.train, scale. = T)
@@ -111,7 +122,8 @@ plot(cumsum(prop_varex), xlab = "Principal Component",
      ylab = "Cumulative Proportion of Variance Explained",
      type = "b")
 #On fait des boxplots pour voir les variables
-box<-boxplot(confood1$saleb1)
+box<-boxplot(sum$saleb1)
 box$out
-b2<-confood1$saleb1[-which(confood1$saleb1%in%box$out)]
+b2<-sum$saleb1[-which(sum$saleb1%in%box$out)]
 boxplot(b2)
+newConfood <- subset(sum, !(sum$saleb1%in%box$out))
